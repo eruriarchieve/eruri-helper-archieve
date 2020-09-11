8b7aa061349311b1691bc8ea38a1969a0ebdf4e3
Goorm <cstria0106@gmail.com>
2020-09-11 23:07:26 +0900

    프로그램이 자동으로 종료되지 않도록 변경

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,4 @@ import createApp from './create-app';
 
   process.stdout.write('http://localhost:3000/\n');
   await open('http://localhost:3000/sign', { wait: true }); // 브라우저로 열기
-
-  process.exit(0); // 종료
 })();
